a450ba89a67ad800fa9bde1c5b0b2f0b0026fe00
Roshan Singh <roshan@roshansingh.dev>
2026-06-11 16:12:32 -0400

    feat: add account API client and MSW handlers

--- test/mocks/handlers.ts
+++ test/mocks/handlers.ts
@@ -2,10 +2,12 @@ import { authHandlers } from './handlers/auth'
 import { emailHandlers } from './handlers/emails'
 import { phoneHandlers } from './handlers/phones'
 import { apiKeyHandlers } from './handlers/api-keys'
+import { accountHandlers } from './handlers/account'
 
 export const handlers = [
   ...authHandlers,
   ...emailHandlers,
   ...phoneHandlers,
   ...apiKeyHandlers,
+  ...accountHandlers,
 ]
